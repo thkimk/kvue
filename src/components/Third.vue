086b2dc8197f56242b823fa92929659c28a8293b
DESKTOP-49P9B8Q\김태헌 <0707thkim@gmail.com>
2021-08-31 23:02:00 +0900

    작업 추가

---
+++ src/components/Third.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="comp comp-tcolor">
+    <p class="comp-name">Third Component</p>
+    <p>props : {{ name }}</p>
+
+    <span class="inject-text">inject props :
+      <span class="inject-value">{{ injectProps }}</span>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'third',
+  props: {
+    name: String
+  },
+  inject: ['injectProps']
+}
+</script>
+
+<style>
+.inject-text {
+  font-weight: bold;
+}
+
+.inject-value {
+  color: red
+}
+</style>
